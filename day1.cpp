6684895c1c74116b2a6d209657cbabba60c3ebfc
Zachary Meadows <zmeadows@gmail.com>
2021-12-01 21:13:42 -0800

    added day2 solution

--- day1.cpp
+++ day1.cpp
@@ -1,13 +1,14 @@
 #include "common.h"
 
-void day1() {
+void day1()
+{
 	const std::vector<S64> depths = read_file_lines_as<S64>("1a.txt");
 
 	{ // start part one
 		U64 answer = 0;
 
 		for (size_t idx = 1; idx < depths.size(); idx++) {
-			answer += depths[idx] > depths[idx - 1];
+			answer += static_cast<U64>(depths[idx] > depths[idx - 1]);
 		}
 
 		std::cout << "1.1) " << answer << std::endl;
@@ -19,13 +20,11 @@ void day1() {
 		S64 last_sum = depths[0] + depths[1] + depths[2];
 		for (size_t idx = 3; idx < depths.size(); idx++) {
 			const S64 next_sum = depths[idx] + depths[idx - 1] + depths[idx - 2];
-			answer += next_sum > last_sum;
+			answer += static_cast<U64>(next_sum > last_sum);
 			last_sum = next_sum;
 		}
 
 		std::cout << "1.2) " << answer << std::endl;
 	} // end part two
-
-	std::cout << std::endl;
 }
 
--- day2.cpp
+++ day2.cpp
@@ -1,5 +1,55 @@
 #include "common.h"
 
-void day2() {
-	std::cout << std::endl;
+void day2()
+{
+	const std::vector<std::string> input = read_file_lines_as<std::string>("2a.txt");
+
+	{ // start part one
+		S64 position = 0;
+		S64 depth = 0;
+
+		for (size_t idx = 0; idx < input.size() - 1; idx += 2) {
+			const std::string& instruction = input[idx];
+			const S64 X = std::stoll(input[idx+1]);
+
+			if (instruction == "forward") {
+				position += X;
+			}
+			else if (input[idx] == "up") {
+				depth -= X;
+			}
+			else if (input[idx] == "down") {
+				depth += X;
+			}
+			else {
+				throw;
+			}
+		}
+
+		std::cout << "2.1) " << depth * position << std::endl;
+	} // end part one
+
+	{ // start part two
+		S64 aim = 0;
+		S64 position = 0;
+		S64 depth = 0;
+
+		for (size_t idx = 0; idx < input.size() - 1; idx += 2) {
+			const std::string& instruction = input[idx];
+			const S64 X = std::stoll(input[idx+1]);
+
+			if (instruction == "forward") {
+				position += X;
+				depth += aim * X;
+			}
+			else if (instruction == "up") {
+				aim -= X;
+			}
+			else if (instruction == "down") {
+				aim += X;
+			}
+		}
+
+		std::cout << "2.2) " << depth * position << std::endl;
+	} // end part two
 }
