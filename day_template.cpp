c66b2ad770c1d0a901c2ca6e33e929d35393df02
Zachary Meadows <zmeadows@gmail.com>
2021-12-05 18:36:36 -0800

    test

--- day_template.cpp
+++ day_template.cpp
@@ -5,12 +5,10 @@ void day6()
 	// const auto depths = read_lines_as<S64>("1a.txt");
 
 	{ // start part one
-
 		print("N.1) {}\n", 0);
 	} // end part one
 
 	{ // start part two
-
 		print("N.2) {}\n", 0);
 	} // end part two
 
